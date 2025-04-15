5b4c1a82125d022789853fcaf8ca0048a3541d6e
Andrii Polonskyi <apolonskyi@alteryx.com>
2025-04-15 15:04:36 +0200

    lesson-24-add-ons

--- lesson-22-23-24/tests-qaauto/cat-site-settings.spec.js
+++ lesson-22-23-24/tests-qaauto/cat-site-settings.spec.js
@@ -1,14 +1,11 @@
 import { test, expect, request } from '@playwright/test';
 import { SettingsPage } from '../poms/qaAutoPoms/pages';
-import { LeftNavBar } from '../poms/qaAutoPoms/sharedComponents';
 
 
 let settingsPage;
-let navBar;
 test.describe('Settings page tests', () => {
   test.beforeEach(async({ page, context }) => {
     settingsPage = new SettingsPage(page);
-    navBar = new LeftNavBar(page);
     await page.goto('https://guest:welcome2qauto@qauto.forstudy.space');
     await expect(page.getByRole('button', { name: 'Sign In' })).toBeVisible();
     await page.getByRole('button', { name: 'Sign In' }).click();
@@ -40,24 +37,51 @@ test.describe('Settings page tests', () => {
   });
 
   test('Can Update currency, units and assert on invalid Change Email or Password actions', async() => {
-    // await settingsPage.openPage();
-    await navBar.selectNavItem('settings');
-    await settingsPage.page.reload();
-    await settingsPage.page.pause();
-    await settingsPage.page.waitForTimeout(2000);
-    expect(await settingsPage.isCurrencySelected('USD')).toBeTruthy();
-    expect(await settingsPage.areUnitsSelected('km')).toBeTruthy();
-    await settingsPage.updateMetrics('EUR', 'ml');
-    await expect(settingsPage.selectors.snackBar('Currency changed')).toBeVisible();
-    await expect(settingsPage.selectors.snackBar('Units of distance changed')).toBeVisible();
-    expect(await settingsPage.isCurrencySelected('EUR')).toBeTruthy();
-    expect(await settingsPage.areUnitsSelected('ml')).toBeTruthy();
+    await test.step('Initial assertion of default state',async() => {
+      await settingsPage.openPage();
+      await settingsPage.page.reload();
+      await settingsPage.page.waitForTimeout(2000);
+      await settingsPage.isCurrencySelected('USD');
+      await settingsPage.areUnitsSelected('km');
+    });
 
-    await settingsPage.changeEmail();
-    await expect(settingsPage.selectors.validationError('Email required')).toBeVisible();
-    await expect(settingsPage.selectors.validationError('Password required')).toBeVisible();
+    await test.step('Update metrics and make sure it worked correctly',async() => {
+      await settingsPage.updateMetrics('EUR', 'ml');
+      await expect(settingsPage.selectors.snackBar('Currency changed')).toBeVisible();
+      await expect(settingsPage.selectors.snackBar('Units of distance changed')).toBeVisible();
+      await settingsPage.page.reload();
+      await settingsPage.isCurrencySelected('EUR');
+      await settingsPage.areUnitsSelected('ml');
+    });
+  
+    await test.step('Verify change email validation errors',async() => {
+      await settingsPage.changeEmail();
+      await expect(settingsPage.selectors.validationError('Email required')).toBeVisible();
+      await expect(settingsPage.selectors.validationError('Password required')).toBeVisible();
+    });
 
-    await settingsPage.changePassword('testHillel1!', 'testHillel1!');
-    await expect(settingsPage.selectors.snackBar('New password should not be the same')).toBeVisible();
+
+    await test.step('Verify change email validation errors',async() => {
+      await settingsPage.changeEmail();
+      await expect(settingsPage.selectors.validationError('Email required')).toBeVisible();
+      await expect(settingsPage.selectors.validationError('Password required')).toBeVisible();
+    });
+
+
+    await test.step('Verify change password validation errors and invalid payload',async() => {
+
+      await settingsPage.changePassword();
+      await expect(settingsPage.selectors.validationError('Old password required')).toBeVisible();
+      await expect(settingsPage.selectors.validationError('New password required')).toBeVisible();
+      await expect(settingsPage.selectors.validationError('Re-enter password required')).toBeVisible();
+
+      await settingsPage.changePassword('faketest');
+      await expect(settingsPage.selectors.validationError('Old password required')).toBeHidden();
+      await expect(settingsPage.selectors.validationError('New password required')).toBeVisible();
+      await expect(settingsPage.selectors.validationError('Re-enter password required')).toBeVisible();
+  
+      await settingsPage.changePassword('testHillel1!', 'testHillel1!');
+      await expect(settingsPage.selectors.snackBar('New password should not be the same')).toBeVisible();
+    });
   });
 });
